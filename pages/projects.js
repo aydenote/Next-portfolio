8343f635a16be48adba514c8739864a1c1e133a5
aydenote <dslgpgh@gmail.com>
2023-03-24 18:39:55 +0900

    서버사이드 렌더링 함수를 통해 내 프로젝트 데이터 구현

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,50 @@
 import Layout from '@/components/layout';
+import ProjectItem from '../components/projects/project-item';
+import { ThemeProvider } from 'next-themes';
+import { DATABASE_ID, TOKEN } from '../config';
 
-export default function Projects() {
+export default function Projects({ projectList }) {
   return (
-    <Layout>
-      <h1>프로젝트</h1>
-    </Layout>
+    <ThemeProvider>
+      <Layout>
+        <div className="flex flex-col items-center justify-center min-h-screen px-6 mb-10">
+          <h1 className="text-4xl font-bold sm:text-6xl">
+            총 프로젝트 :<span className="pl-4 text-blue-500">{projectList.results.length}</span>
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 w-full">
+            {projectList.results.map(project => (
+              <ProjectItem key={project.id} project={project} />
+            ))}
+          </div>
+        </div>
+      </Layout>
+    </ThemeProvider>
   );
+}
+
+export async function getServerSideProps(context) {
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+      authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      page_size: 100,
+      sorts: [
+        {
+          property: '기간',
+          direction: 'descending',
+        },
+      ],
+    }),
+  };
+
+  const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
+  const projectList = await res.json();
+  return {
+    props: { projectList }, // will be passed to the page component as props
+  };
 }
